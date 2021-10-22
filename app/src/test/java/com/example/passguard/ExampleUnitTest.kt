09d887406088858d06b57aadb1d041c69a6deaed
kyd3snik <kartashovandrey73@gmail.com>
2021-10-22 21:20:51 +0300

    Initial commit

---
+++ app/src/test/java/com/example/passguard/ExampleUnitTest.kt
@@ -0,0 +1,18 @@
+package com.example.passguard
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val actual = 2 + 2 * 2
+        val expected = 6
+        assertEquals(expected, actual)
+    }
+}
